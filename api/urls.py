66ebc95648f5054916264f58394a7c43ebd59abf
syedwajahath11 <syedwajahath11@gmail.com>
2021-01-20 07:57:32 +0530

    template

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.urls.conf import re_path
 from .views import *
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('qurydata/',search_result.as_view())
 ]
-
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='upwork/build/index.html'))]
